b067dd52b9d09fe1454e06380d484f9e0d559113
miinjung <laura126@ajou.ac.kr>
2020-10-10 23:57:18 +0900

    for error

--- app/src/main/java/com/junction/mymusicmap/presentation/main/MainActivity.kt
+++ app/src/main/java/com/junction/mymusicmap/presentation/main/MainActivity.kt
@@ -1,19 +1,20 @@
-package com.junction.mymusicmap.presentation
+package com.junction.mymusicmap.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.junction.mymusicmap.R
 import com.junction.mymusicmap.presentation.musicsearch.MusicSearchDialogFragment
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnMusicSearch.setOnClickListener {
-            val bottomSheetFragment = MusicSearchDialogFragment()
-            bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
-        }
+
+    }
+
+    private fun pinClickEvent() {
+        val bottomSheetFragment = MusicSearchDialogFragment()
+        bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
     }
 }
---
+++ app/src/main/java/com/junction/mymusicmap/presentation/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.junction.mymusicmap.presentation.main
+
+import androidx.lifecycle.ViewModel
+import com.junction.mymusicmap.presentation.musicsearch.MusicSearchDialogFragment
+
+class MainViewModel : ViewModel() {
+
+}
